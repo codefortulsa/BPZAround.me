0b59f0e7e318f3130cb67dc26a28cfde04abd165
John Whitlock <John-Whitlock@ieee.org>
2014-08-22 16:44:17 -0500

    Add frontend skeleton
    - Bootstrap 3.20 and JQuery 1.11.1
    - Jinja2 base and home template
    - Jingo for Django integration with Jinja2
    - Placeholders for JS and CSS
    - Homepage view

--- bpz/urls.py
+++ bpz/urls.py
@@ -1,5 +1,11 @@
-from django.conf.urls import patterns
+'''Views for bpz'''
+from django.conf.urls import patterns, url
+
+from .views import RequestView
 
 urlpatterns = patterns(
-    ''
+    '',
+    url(r'^$', RequestView.as_view(
+        template_name='bpz/home.jinja2'),
+        name='home'),
 )
--- bpz/views.py
+++ bpz/views.py
@@ -1,1 +1,12 @@
-'''Views for bpz'''
+from django.views.generic import TemplateView
+
+
+class RequestContextMixin(object):
+    def get_context_data(self, **kwargs):
+        ctx = super(RequestContextMixin, self).get_context_data(**kwargs)
+        ctx['request'] = self.request
+        return ctx
+
+
+class RequestView(RequestContextMixin, TemplateView):
+    pass
