61dd691bfc76c28ade1af4586f615b85183e7aa5
Tom White <whit@johnchristner.com>
2014-08-26 10:46:55 -0500

    Basic model for subscriptions and notifications
    Make environment var pulling more robust, fix pulling settings

--- notifier/views.py
+++ notifier/views.py
@@ -11,6 +11,10 @@
         Handle incoming SMS messages
 
 '''
+from twilio.rest import TwilioRestClient
+from django.conf import settings
+from django.views.generic import TemplateView
+
 
 class RequestContextMixin(object):
     def get_context_data(self, **kwargs):
@@ -18,6 +22,22 @@ class RequestContextMixin(object):
         ctx['request'] = self.request
         return ctx
 
+
+def SendText(self, phone, smstext):
+    '''
+    Send a text message using twilio
+
+    :param phone:
+    :param smstext:
+    :return:
+    '''
+    client = TwilioRestClient(settings.TWILIO_SID, settings.TWILIO_AUTH)
+
+    client.messages.create(
+        body=smstext,
+        to=phone,
+        from_=settings.TWILIO_NUM
+    )
 
 def changeSettings(request, nonce=""):
     '''Use emailed or texted URL to modify settings
