7680dc8f4d4adaf51587afb73d646caa592f5a9d
Paul Stefan Ort <mail@paulstefanort.com>
2014-08-27 22:11:33 -0500

    Add “About” page.

--- bpz/urls.py
+++ bpz/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns(
     url(r'^$', TemplateView.as_view(
         template_name='bpz/home.jinja2'),
         name='home'),
+    url(r'^about', TemplateView.as_view(
+        template_name='bpz/about.jinja2'),
+        name='about'),
 )
