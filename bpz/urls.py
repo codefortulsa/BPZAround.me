fe1e8b823258cd5df823cc4de9a6c54ca4c3efd3
jdungan <johnadungan@gmail.com>
2014-08-29 15:15:38 -0500

    flake8 cleanup

--- bpz/urls.py
+++ bpz/urls.py
@@ -3,22 +3,27 @@ from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 from rest_framework import routers
 
-from .views import CaseViewSet, HOAViewSet
+from .views import CaseViewSet, HOAViewSet, cases
 
 
 router = routers.DefaultRouter()
 router.register(r'cases', CaseViewSet)
 router.register(r'hoas', HOAViewSet)
-from django.views.generic import TemplateView
-
-from .views import cases
 
 urlpatterns = patterns(
     '',
     url(r'^api/', include(router.urls)),
-    url(r'^$', TemplateView.as_view(template_name='bpz/home.jinja2'),name='home'),
-    url(r'^about', TemplateView.as_view(template_name='bpz/about.jinja2'),name='about'),
-    url(r'^hood', TemplateView.as_view(template_name='bpz/hood.jinja2'),name='neighborhood'),
-    url(r'^notify', TemplateView.as_view(template_name='bpz/notify.jinja2'),name='notify'),
+    url(r'^$',
+        TemplateView.as_view(template_name='bpz/home.jinja2'),
+        name='home'),
+    url(r'^about',
+        TemplateView.as_view(template_name='bpz/about.jinja2'),
+        name='about'),
+    url(r'^hood',
+        TemplateView.as_view(template_name='bpz/hood.jinja2'),
+        name='neighborhood'),
+    url(r'^notify',
+        TemplateView.as_view(template_name='bpz/notify.jinja2'),
+        name='notify'),
     url(r'^cases', cases, name='cases'),
 )
