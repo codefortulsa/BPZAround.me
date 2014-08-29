fc89481ff23b8d9af6f95b4ccddd79398d22ed8d
jdungan <johnadungan@gmail.com>
2014-08-29 18:08:44 -0500

    load case geojson from api and add click behavior

--- bpz/static/js/bpz.js
+++ bpz/static/js/bpz.js
@@ -31,8 +31,43 @@
 //
 //
 
+
+var call_map, bpz, value, _fn, _i, _len;
+
+bpz = {
+  api: {}
+};
+
+
+bpz.api.raw = function(resource, ajax_params) {
+  ajax_params = ajax_params || {};
+  return $.ajax({
+    type: "get",
+    url: "/api" + resource,
+    data: ajax_params,
+    dataType: "json"
+  });
+};
+
+call_map = [["cases", "/cases"]];
+
+_fn = function(value) {
+  return bpz.api[value[0]] = function(params) {
+    return bpz.api.raw(value[1], params);
+  };
+};
+
+for (_i = 0, _len = call_map.length; _i < _len; _i++) {
+  value = call_map[_i];
+  _fn(value);
+}
+
+document.dispatchEvent(new Event('bpz_load'))
+
 jQuery(document).ready(function () {
+
   L.mapbox.accessToken ='pk.eyJ1IjoiamR1bmdhbiIsImEiOiJlOTl6MFpNIn0.-3o5vIOCjkfXd-7ibZrb8A'
   map = L.mapbox.map('map-canvas', 'jdungan.jbbebonl').setView([36.1587336,-95.9940543],16);
+  $(window).trigger('bpz_load');
     
 });
