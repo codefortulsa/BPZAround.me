a5ea263b44bef2cdf8b6b90a8bee595407f6397b
jdungan <johnadungan@gmail.com>
2014-09-03 19:25:47 -0500

    fix lint issues

--- bpz/urls.py
+++ bpz/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 from rest_framework import routers
 
-from .views import CaseViewSet, HOAViewSet, cases,hoa
+from .views import CaseViewSet, HOAViewSet, cases, hoa
 
 
 router = routers.DefaultRouter()
--- bpz/views.py
+++ bpz/views.py
@@ -20,5 +20,7 @@ class HOAViewSet(viewsets.ModelViewSet):
 def cases(request):
     return render(request, 'bpz/cases.jinja2', {'cases': Case.objects.all()})
 
+
 def hoa(request):
-    return render(request, 'bpz/hoa.jinja2', {'assocs': HomeOwnersAssociation.objects.all()})
+    return render(request, 'bpz/hoa.jinja2', 
+        {'assocs': HomeOwnersAssociation.objects.all()})
