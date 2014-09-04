2ff0dc12bbfd19ba59511afbf5cfd80f596494a1
jdungan <johnadungan@gmail.com>
2014-09-04 07:47:48 -0500

    fixes based on comments, added TODO's for later fixes

--- bpz/static/js/bpz.js
+++ bpz/static/js/bpz.js
@@ -1,12 +1,12 @@
 var call_map, bpz, value, _fn, _i, _len;
 
 
+
+//TODO: replace 
 function Location() {
-  var options = {
-    enableHighAccuracy: true
-  },
-    inner_pos = {},
+  var options = {enableHighAccuracy: true},
     dfd = new $.Deferred(),
+    inner_pos = {}
     moved = function(pos) {
       inner_pos = pos || {};
       dfd.resolve(this)
@@ -17,42 +17,43 @@ function Location() {
       }
       dfd.reject()
     };
-
-  this.__defineGetter__("lat", function() {
-    return inner_pos.coords ? inner_pos.coords.latitude : null;
-  });
-
-  this.__defineGetter__("lng", function() {
-    return inner_pos.coords ? inner_pos.coords.longitude : null;
-  });
-
-  this.__defineGetter__("pos", function() {
-      return {lng:this.lng,lat:this.lat};
+    
+    this.__defineGetter__("lat", function() {
+      return inner_pos.coords ? inner_pos.coords.latitude : null;
     });
+
+    this.__defineGetter__("lng", function() {
+      return inner_pos.coords ? inner_pos.coords.longitude : null;
+    });
+
+    this.__defineGetter__("pos", function() {
+        return {lng:this.lng,lat:this.lat};
+      });
+  this.position = inner_pos;
 
   this.WatchID = navigator.geolocation.watchPosition(moved, fail, options);
 
   this.ready = dfd.promise()
-}
 
+}
 
 
 bpz = {
   _map:false,
   api: {},
-  'location': new Location()
+  'location': new Location(),
+  get map() {
+    if (!bpz._map){
+      //TODO: Move access token into environment variable 
+      L.mapbox.accessToken ='pk.eyJ1IjoiamR1bmdhbiIsImEiOiJlOTl6MFpNIn0.-3o5vIOCjkfXd-7ibZrb8A'
+      bpz._map=L.mapbox.map('map-canvas', 'jdungan.jbbebonl').setView([36.1587336,-95.9940543],12); 
+    }
+    return bpz._map
+  }
 };
 
-bpz.__defineGetter__("map", function() {
-  if (!bpz._map){
-    L.mapbox.accessToken ='pk.eyJ1IjoiamR1bmdhbiIsImEiOiJlOTl6MFpNIn0.-3o5vIOCjkfXd-7ibZrb8A'
-    bpz._map=L.mapbox.map('map-canvas', 'jdungan.jbbebonl').setView([36.1587336,-95.9940543],12); 
-  }
-  return bpz._map
-});
 
-
-bpz.api.raw = function(resource, ajax_params) {
+bpz.api.call = function(resource, ajax_params) {
   ajax_params = ajax_params || {};
   return $.ajax({
     type: "get",
@@ -66,7 +67,7 @@ call_map = [["cases", "/cases"],["hoa", "/hoas"]];
 
 _fn = function(value) {
   return bpz.api[value[0]] = function(params) {
-    return bpz.api.raw(value[1], params);
+    return bpz.api.call(value[1], params);
   };
 };
 
