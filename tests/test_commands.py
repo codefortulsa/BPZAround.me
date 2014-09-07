061a5c35bc6d70b26ccefc376ebfbd1136fede02
groovecoder <luke.crouch@gmail.com>
2014-09-07 18:57:21 -0500

    update tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -5,13 +5,17 @@ try:
 except ImportError:
     from mock import patch, MagicMock
 
+from django.contrib.gis.gdal import DataSource
 from django.test import TestCase
 
 from bpz.management.commands.load_cases import (parse_datetime,
-                                                parse_raw_value)
+                                                parse_feature_raw_value)
 
 
 class ParseTestCase(TestCase):
+
+    def setUp(self):
+        self.ds = DataSource('tests/fixtures/test-cases.json')
 
     def test_parse_datetime(self):
         value = "July 8, 2014"
@@ -19,14 +23,14 @@ class ParseTestCase(TestCase):
         eq_(expected, parse_datetime(value))
 
     def test_parse_raw_value_plain(self):
-        value = "City Council Chambers"
+        feature = self.ds[0][0]  # plain feature
         expected = "City Council Chambers"
-        eq_(expected, parse_raw_value(value))
+        eq_(expected, parse_feature_raw_value(feature, 'Location'))
 
     @patch("bpz.management.commands.load_cases.stderr")
     def test_parse_raw_value_with_newlines(self, stderr):
         stderr.write = MagicMock()
-        value = "City Council Chambers\r\n\r\nCouncil Chambers"
+        feature = self.ds[0][1]  # newlines feature
         expected = "City Council Chambers"
-        eq_(expected, parse_raw_value(value))
+        eq_(expected, parse_feature_raw_value(feature, 'Location'))
         ok_(stderr.write.called)
