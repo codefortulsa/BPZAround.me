75cdb9dc28de34be4694197403d06aecd3970643
Brent Gardner <btgardner@gmail.com>
2014-09-13 15:01:19 -0500

    added scroll-spy effect to panel lists

--- bpz/static/js/bpz.js
+++ bpz/static/js/bpz.js
@@ -91,7 +91,7 @@ bpz = {
   lists:{
     build: function (selector,data) {
       var ul = d3.select(selector)
-      ul.style({position:'relative',top:'300px'})
+      //ul.style({position:'relative',top:'300px'})
       
       //create an li for all features
       li = ul.selectAll("li")
